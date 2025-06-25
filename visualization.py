e2ebeb42fcc6ec40da60f71c64ff919f20c4412f
k0ssie <shirleen.kosgei@strathmore.edu>
2025-06-25 16:21:29 +0300

    Added my Visuals Module

---
+++ visualization.py
@@ -0,0 +1,6 @@
+def save_sanpshot(self):
+        with open("queue_snapshot.txt","a")as f:
+            f.write(f"Snapshot at Tick{self.time}:\n")
+            for job in self.queue:
+                f.write(f"{job["job_id"]} - {job["user_id"]} - P:{job["priority"]} - WT:{job["waiting_time"]}\n")
+            f.write("-" * 30 + "\n")
