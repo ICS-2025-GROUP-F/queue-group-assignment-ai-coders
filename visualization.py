49110e615df0cfe59d3f011aadf58db9ef4e5b21
K0ssie <shirleenkosgei306@gmail.com>
2025-07-01 09:30:25 +0300

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,6 +1,12 @@
 def save_snapshot(self):
-        with open("queue_snapshot.txt","w")as f:
-            f.write(f"Snapshot at Tick{self.time}:\n")
-            for job in self.queue:
-                f.write(f"{job['job_id']} - {job['user_id']} - P:{job['priority']} - WT:{job['waiting_time']}\n")
+    with self.lock:
+        with open("queue_snapshot.txt", "w") as f:
+            f.write(f"Snapshot at Tick {self.time}:\n")
+            index = self.front
+            count = 0
+            while count < self.size:
+                job = self.queue[index]
+                f.write(f"{job['job_id']} - {job['user_id']} - P:{job['priority']} - WT:{job['waiting_time']} - ST:{job['submitted_time']}\n")
+                index = (index + 1) % self.capacity
+                count += 1
             f.write("-" * 30 + "\n")
