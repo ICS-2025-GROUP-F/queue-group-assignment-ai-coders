3b9179bc76f924b9dc7f6384702cb99870f27dc0
K0ssie <shirleenkosgei306@gmail.com>
2025-07-01 09:12:42 +0300

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,5 +1,5 @@
-def save_sanpshot(self):
-        with open("queue_snapshot.txt","a")as f:
+def save_snapshot(self):
+        with open("queue_snapshot.txt","w")as f:
             f.write(f"Snapshot at Tick{self.time}:\n")
             for job in self.queue:
                 f.write(f"{job['job_id']} - {job['user_id']} - P:{job['priority']} - WT:{job['waiting_time']}\n")
